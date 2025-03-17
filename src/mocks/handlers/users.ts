7b047224392434b9934df5c2fda06114bba3f288
Mohammad Tarek <tarek3529@gmail.com>
2025-03-17 04:42:09 +0200

    add user management handlers for fetching, updating, and deleting users

--- src/mocks/handlers/users.ts
+++ src/mocks/handlers/users.ts
@@ -3,12 +3,6 @@ import { mockUsers } from "../mockData/mockUsers"
 
 const delayInRange = Math.floor(Math.random() * (800 - 300 + 1)) + 300;
 
-// interface UserParams {
-//   page?: string;
-//   pageSize?: string;
-//   id?: string;
-// }
-
 export const handlers = [
   http.get('/api/users', async ({ request }) => {
     await delay(delayInRange)
@@ -22,7 +16,9 @@ export const handlers = [
     const filteredUsers = mockUsers.filter(user => {
       const { name, role, status } = filters;
 
-      return (name ? user.name === name : true) && (role ? user.role === role : true) && (status ? user.status === status : true)
+      return (!name || user.name === name) &&
+        (!role || user.role === role) &&
+        (!status || user.status === status)
     })
 
     const startIndex = (page - 1) * pageSize;
@@ -38,11 +34,51 @@ export const handlers = [
       filters: JSON.stringify(filters),
     });
   }),
-  http.get('/api/users/:id', ({ params }) => {
+  http.get('/api/users/:id', async ({ params }) => {
+    await delay(delayInRange);
+
     const user = mockUsers.find((u) => u.id === Number(params.id));
     return user ? HttpResponse.json(user) : new HttpResponse(null, {
       status: 404,
-      statusText: 'Out Of Apples'
+      statusText: 'User Not Found'
     })
   }),
+  http.put('/api/users/:id', async ({ params, request }) => {
+    await delay(delayInRange);
+
+    const userIndex = mockUsers.findIndex((u) => u.id === Number(params.id));
+
+    if (userIndex === -1) {
+      return new HttpResponse(null, {
+        status: 404,
+        statusText: 'User Not Found'
+      });
+    }
+
+    const updatedData = await request.json();
+    if (typeof updatedData === 'object' && updatedData !== null) {
+      mockUsers[userIndex] = { ...mockUsers[userIndex], ...updatedData };
+    }
+
+    return HttpResponse.json(mockUsers[userIndex]);
+  }),
+
+  http.delete('/api/users/:id', ({ params }) => {
+    const userIndex = mockUsers.findIndex((u) => u.id === Number(params.id));
+
+    if (userIndex === -1) {
+      return new HttpResponse(null, {
+        status: 404,
+        statusText: 'User Not Found'
+      });
+    }
+
+    // remove user from the list
+    mockUsers.splice(userIndex, 1);
+
+    return new HttpResponse(null, {
+      status: 204,
+    });
+  }),
+
 ];
