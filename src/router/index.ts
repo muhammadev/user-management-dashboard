18178a9e22214d0ec6e9ebe918f30a98f24fddb7
Mohammad Tarek <tarek3529@gmail.com>
2025-03-17 04:40:07 +0200

    refactor router to use DefaultLayout for improved structure and organization

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DashboardView from '../views/DashboardView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'dashboard',
-      component: DashboardView,
-    },
-    {
-      path: '/user/:id',
-      name: 'user-details',
-      component: () => import('../views/UserDetailsView.vue'),
+      name: 'DefaultLayout',
+      component: DefaultLayout,
+      children: [
+        {
+          path: "",
+          component: DashboardView,
+        },
+        {
+          path: '/user/:id',
+          component: () => import('../views/UserDetailsView.vue'),
+        },
+
+      ]
     },
   ],
 })
