2df83c9eaa98732525b5b14d20a0c6324809f1a6
Mohammad Tarek <tarek3529@gmail.com>
2025-03-18 20:49:29 +0200

    add session expiration warning and reset logic in useSessionTimeout

--- src/composables/useSessionTimeout.ts
+++ src/composables/useSessionTimeout.ts
@@ -1,10 +1,12 @@
 import { useAuthStore } from "@/stores/authStore";
 import { useToast } from "primevue";
-import { onMounted, onUnmounted } from "vue"
+import { onMounted, onUnmounted, ref } from "vue"
 
 export function useSessionTimeout() {
   const authStore = useAuthStore();
   const toast = useToast();
+
+  const isWarned = ref(false);
 
   let interval: ReturnType<typeof setInterval>;
 
@@ -14,17 +16,20 @@ export function useSessionTimeout() {
     const now = Date.now();
 
     // give warning before expiration by 1 minute
-    if (now >= (authStore.sessionExpiresAt - 60000)) {
+    if (now >= (authStore.sessionExpiresAt - 60000) && !isWarned.value) {
       toast.add({
         severity: "warn",
         summary: "Warning",
         detail: "Your session will expire in 1 minute.",
         life: 3000
-      })
+      });
+
+      isWarned.value = true;
     }
 
     if (now >= authStore.sessionExpiresAt) {
       authStore.logout();
+      isWarned.value = false;
     }
   }
 
